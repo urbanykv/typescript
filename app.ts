98355ace7811b564d78bcf178a3ac2ed57bb2905
urbanykv <matheusurban19@gmail.com>
2024-01-10 01:16:54 -0300

    POO e tipagem forte

--- app.ts
+++ app.ts
@@ -29,3 +29,30 @@ const typeUser = {
 function validateUser(user: string){
   console.log(typeUser[user as keyof typeof typeUser]);
 }
+
+const Matheus = {
+  name: 'Matheus',
+  age: 25,
+  shownApresentation(){console.log(`Oi, meu nome é ${this.name}`)}
+}
+
+
+class Pessoa{
+  private name;
+  private age;
+
+  constructor(name: string, age: number){
+    this.name = name;
+    this.age = age;
+  }
+
+  showApresentation(){
+    console.log(`Oi, meu nome é ${this.name} e eu tenho ${this.age} anos!`);
+    
+  }
+}
+
+
+const p1 = new Pessoa("Matheus", 32);
+
+p1.showApresentation();
